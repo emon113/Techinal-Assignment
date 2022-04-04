ebcb66cbccbf2a6aefed5b0fceac5b68a7df7718
Md Mahbubur Rahman <mahbubur.rahman04@northsouth.edu>
2022-04-04 20:08:08 +0600

    Comment added.

--- ImageMatchingPythonScript.py
+++ ImageMatchingPythonScript.py
@@ -7,7 +7,7 @@ search_tags = ["retro","african","svg","cat"]
 for web_page in web_urls:                                   # For loop for iterate over the website URLs
     html_file = requests.get(url=web_page).text             # Getting the HTML file of the webpage.
     soup = BeautifulSoup(html_file,"html.parser")           # Creating an instance of BeautifulSoup with the HTML file and 'html.parser' is used as a parser.
-    images = soup.find_all('img', alt=True)                 # Finding all the img tag with the 'alt'.   
+    images = soup.find_all('img', alt=True)                 # Finding all the img tag along with the 'alt'.   
 
     for image in images:                                    # For loop to iterate over the img tag.
         for tag in search_tags:                             # for loop to iterate over the search tags.
